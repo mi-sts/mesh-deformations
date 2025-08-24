0a709054785d8f7fa9440bbb008771809f5dd70c
Stanislav Mishchenko <mi.sts@yandex.ru>
2025-08-24 20:07:51 +0300

    add the libigl dependency

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
-#include <iostream>
+#include <igl/opengl/glfw/Viewer.h>
+#include <Eigen/Core>
 
 int main() {
-    return 0;
+    Eigen::MatrixXd V(4,3);
+    Eigen::MatrixXi F(2,3);
+
+    V << 0,0,0, 1,0,0, 1,1,0, 0,1,0;
+    F << 0,1,2, 0,2,3;
+
+    igl::opengl::glfw::Viewer viewer;
+    viewer.data().set_mesh(V,F);
+    viewer.launch();
 }
